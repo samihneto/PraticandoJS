270153e230b7c8e061a6a2f33b0d2342b805cd36
Samir Neto <samihneto@gmail.com>
2024-12-30 13:31:12 -0300

    Primeira leva de exercícios

---
+++ ConhecendoArrays/calculoMedia.js
@@ -0,0 +1,15 @@
+// método tradicional
+// const nota1 = 10;
+// const nota2 = 6.5;
+// const nota3 = 8;
+// const nota4 = 7.5;
+
+// const media = (nota1+nota2+nota3+nota4)/4;
+// console.log(media);
+
+// método com array
+
+const notas = [10, 6.5, 8, 7.5];
+
+const media = (notas[0] + notas[1] + notas[2] + notas[3])/notas.length;
+console.log(media);
